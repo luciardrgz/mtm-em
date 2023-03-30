48248521c6b3841f2528f9986e1e2882f6856778
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-03-30 19:29:59 -0300

    Exporting constants to use in app

---
+++ utils/constants.js
@@ -0,0 +1,54 @@
+export const navLinks = [
+  {
+    id: 'home',
+    title: 'Inicio',
+  },
+  {
+    id: 'services',
+    title: 'Servicios',
+  },
+  {
+    id: 'customers',
+    title: 'Clientes',
+  },
+  {
+    id: 'contact',
+    title: 'Contacto',
+  },
+];
+
+export const slides = [
+  {
+    url: 'https://i.ibb.co/sQY8kwr/industria1.jpg',
+  },
+  {
+    url: 'https://i.ibb.co/CJ1NK4m/industria2.jpg',
+  },
+  {
+    url: 'https://i.ibb.co/ypX9rCv/industria3.jpg',
+  },
+  {
+    url: 'https://i.ibb.co/b2yff4N/industria4.jpg',
+  },
+  {
+    url: 'https://i.ibb.co/ZTmjgtH/industria5.jpg',
+  },
+];
+
+export const stats = [
+  {
+    id: 'stat1',
+    title: 'Años de experiencia',
+    value: '+15',
+  },
+  {
+    id: 'stat2',
+    title: 'Empresas que confían',
+    value: '+20',
+  },
+  {
+    id: 'stat3',
+    title: 'Trabajos realizados',
+    value: '+25',
+  },
+];

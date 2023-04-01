c9c19464521b19eb97171fc535693cfa4f45e013
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-04-01 16:23:32 -0300

    Added Sticky Social Bar

---
+++ components/StickyBar.jsx
@@ -0,0 +1,10 @@
+import { SocialIcon } from 'react-social-icons';
+
+const StickyBar = () => (
+  <div className="sticky-bar fixed right-0 top-1/4 w-12 lg:w-14 md:w-14 items-center z-20 p-2 flex flex-col space-y-4 rounded-lg">
+    <SocialIcon url="https://api.whatsapp.com/send/?phone=5492235828183&text&type=phone_number&app_absent=0" fgColor="white" target="_blank" style={{ height: 35, width: 35 }} />
+    <SocialIcon url="https://www.instagram.com/mtm_s.e/" fgColor="white" target="_blank" style={{ height: 35, width: 35 }} />
+    <SocialIcon url="https://www.linkedin.com/in/cristian-pollicino-2311ab111/" fgColor="white" target="_blank" style={{ height: 35, width: 35 }} />
+  </div>
+);
+export default StickyBar;

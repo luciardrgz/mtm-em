b6b883be70fcc9aad9f62a1672da9c866aa3b8a7
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-04-21 19:48:14 -0300

    Added React Helmet

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,45 @@
-import Head from 'next/head';
 import '../styles/globals.css';
+import { Helmet } from 'react-helmet';
 
 const MyApp = ({ Component, pageProps }) => (
   <>
-    <Head>
-      <title>MTM | Soluciones Electromecánicas</title>
+    <Helmet>
+      <script type="application/ld+json">
+        {`
+          {
+            "@context": "https://schema.org",
+            "@type": "Electrician", 
+            "@id": "Electrician",
+            "url": "https://mtmelectromecanica.com",
+            "telephone": "+54 9 223 582 8183",
+            "email": "mtm.solucioneselectromecanicas@gmail.com",
+            "logo": "https://mtmelectromecanica.com/logo.webp",
+            "paymentAccepted": [
+              "Cash",
+              "Credit Card",
+              "Bank Transfer",
+              "Efectivo",
+              "Tarjeta de Crédito",
+              "Transferencia Bancaria"
+            ],
+            "address": "Mar del Plata, Buenos Aires, Argentina",
+            "description": "Electricidad, mecánica y automatización de procesos productivos. Mantenimiento industrial integral. Electricistas listos para trabajar en tu negocio o industria.",
+            "name": "MTM Electromecánica",
+            "openingHours": "Mo, Tu, We, Th, Fr",
+            "areaServed": "Mar del Plata Area",
+            "latitude": "-34.931471",
+            "longitude": "-57.958678"
+          }
+        `}
+      </script>
+
+      <title>
+        MTM Soluciones Electromecánicas | Electricistas en Mar del Plata
+      </title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
         name="description"
-        content="Electricidad, mecánica y automatización de procesos productivos - Mantenimiento industrial integral - Soluciones eléctricas para tu negocio o industria."
+        content="Electricidad, mecánica y automatización de procesos productivos. Mantenimiento industrial integral. Electricistas listos para trabajar en tu negocio o industria."
       />
       <meta
         name="keywords"
@@ -17,31 +48,33 @@ const MyApp = ({ Component, pageProps }) => (
       <meta property="og:title" content="MTM Soluciones Electromecánicas" />
       <meta property="og:type" content="website" />
       <meta property="og:image" content="/logo.png" />
-      <meta property="og:url" content="https://mtm-electromecanica.vercel.app/" />
+      <meta
+        property="og:url"
+        content="https://mtmelectromecanica.com/"
+      />
       <meta
         property="og:description"
-        content="Electricidad, mecánica y automatización de procesos productivos - Mantenimiento industrial integral"
+        content="Electricidad, mecánica y automatización de procesos productivos. Mantenimiento industrial integral. Electricistas listos para trabajar en tu negocio o industria."
       />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content="MTM Soluciones Electromecánicas" />
       <meta
         name="twitter:description"
-        content="Electricidad, mecánica y automatización de procesos productivos - Mantenimiento industrial integral"
+        content="Electricidad, mecánica y automatización de procesos productivos. Mantenimiento industrial integral. Electricistas listos para trabajar en tu negocio o industria."
       />
       <meta name="twitter:image" content="/logo.png" />
 
       <link rel="icon" href="/logo.ico" />
 
-      <link rel="canonical" href="https://mtm-electromecanica.vercel.app/#home" />
+      <link rel="canonical" href="https://mtmelectromecanica.com/" />
 
       <meta name="author" content="Lucía Rodriguez" />
 
       <meta name="publisher" content="MTM Electromecánica" />
-    </Head>
+    </Helmet>
     <Component {...pageProps} />
   </>
-
 );
 
 export default MyApp;
